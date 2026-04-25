456035a759196b1960c5e187758dd6e181d4497a
MD NAWED ALAM <nawedmr477@gmail.com>
2026-04-25 23:52:01 +0530

    oops

--- main.java
+++ main.java
@@ -4,6 +4,17 @@ public class main {
     public static void main(String[] args) {
         int[][] matrix = { { 2, 3, 4, 5, 3, 4 }, { 4, 6, 54, 3, 2, 33 } };
         diagonalOperation(matrix);
+
+        // student original = new student("Nawed", 29);
+        // student copy = new student(original);
+
+        // System.out.println("Original: " + original.name + " " + original.id);
+        // System.out.println("Copy: " + copy.name + " " + copy.id);
+        main obj = new main();
+        student original = obj.new student("Nawed", 29);
+        student copy = obj.new student(original);
+        System.out.println(original);
+
     }
 
     public static void diagonalOperation(int matrix[][]) {
@@ -32,4 +43,21 @@ public class main {
 
         }
     }
+
+    public class student {
+        String name;
+        int id;
+
+        public student(String name, int id) {
+            this.name = name;
+            this.id = id;
+        }
+
+        public student(student other) {
+            this.name = other.name;
+            this.id = other.id;
+        }
+
+    }
+
 }

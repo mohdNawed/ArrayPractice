5ae0c8d55a91126c0591a3e36059a8f115e57928
MD NAWED ALAM <nawedmr477@gmail.com>
2026-05-13 14:19:17 +0530

    array

---
+++ SumOfSubarray.java
@@ -0,0 +1,69 @@
+// package ArrayPractice;
+
+// public class SumOfSubarray {
+
+//     public class subarray {
+//         public static void PrintSubArray(int number[]) {
+//             int currSum = 0;
+//             int maxSum = Integer.MIN_VALUE;
+//             for (int i = 0; i < number.length; i++) {
+//                 int start = i;
+//                 for (int j = i; j < number.length; j++) {
+//                     int end = j;
+//                     currSum = 0;
+//                     for (int k = start; k < end; k++) {
+//                         currSum += number[k];
+//                     }
+//                     System.out.println(currSum);
+//                     if (maxSum < currSum) {
+//                         maxSum = currSum;
+//                     }
+//                 }
+
+//             }
+
+//             System.out.println("Max Sum" + maxSum);
+//         }
+
+//         public static void main(String[] args) {
+//             int number[] = { 2, 4, 6, 7, 8, 10 };
+//             PrintSubArray(number);
+//         }
+
+//     }
+
+// }
+
+package ArrayPractice;
+
+public class SumOfSubarray {
+    public static void printSubArraySum(int number[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < number.length; i++) {
+            int start = i;
+
+            for (int j = i; j < number.length; j++) {
+                int end = j;
+                currSum = 0;
+
+                for (int k = start; k <= end; k++) {
+                    currSum += number[k];
+                }
+
+                System.out.println("Subarray sum: " + currSum);
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("Final Max Sum = " + maxSum);
+    }
+
+    public static void main(String[] args) {
+        int number[] = { 2, 4, 6, 8, 10 };
+        printSubArraySum(number);
+
+    }
+}
